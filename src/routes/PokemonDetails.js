aaeb22e9aab876f886fa7dbb8cebec2fe4a1c95a
rfrias8 <rfrias8@gmail.com>
2022-11-09 12:21:10 -0600

    Added the PokemonDetails function and useEffect

--- src/routes/PokemonDetails.js
+++ src/routes/PokemonDetails.js
@@ -1,6 +1,34 @@
-function PokemonDetails() {
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 
+function PokemonDetails() {
+    const [pokemon, setPokemon] = useState(null);
+    const params = useParams();
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data)
+            setPokemon(data.name);
+            console.log(pokemon)
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
+
+      if (!pokemon) {
+        return <>loading...</>;
+      }
+
+
+      return 
+        <>
+          height: 
+
+        </>
 }
 
 
-export {PokemonDetails}
+export {PokemonDetails};
